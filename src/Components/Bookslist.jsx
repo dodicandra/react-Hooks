7b3dd1762d6728ea8e67c61ce99f754dc3bb2f1d
cekidot <cekiddot@gmail.com>
2020-01-19 22:21:41 +0700

    lesson-1

---
+++ src/Components/Bookslist.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Bookslist extends Component {
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+          <li>react Context API</li>
+          <li>the net ninja</li>
+          <li>the way of kings</li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../Contexts/themeContext';
+
+export default class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
