49768f13b986d43f7520c2c15c723688b6c0a0ac
cekidot <cekiddot@gmail.com>
2020-01-22 23:02:19 +0700

    commit on hooksapp

--- hooksapp/src/Components/SongList.js
+++ hooksapp/src/Components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/v1';
 import NewSongForm from './NewSongForm';
 
@@ -9,9 +9,16 @@ const SongList = () => {
     { title: 'perl', id: 3 },
   ]);
 
+  const [age, setAge] = useState(20);
   const addSong = (title) => {
     setSongs([...songs, { title, id: uuid() }]);
   };
+  useEffect(() => {
+    console.log('use effect run', songs);
+  }, [songs]);
+  useEffect(() => {
+    console.log('use effect run', age);
+  }, [age]);
   return (
     <div className="song-list">
       <ul>
@@ -20,6 +27,7 @@ const SongList = () => {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>add 1 to age : {age}</button>
     </div>
   );
 };
