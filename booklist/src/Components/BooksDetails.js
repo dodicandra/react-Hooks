9041f1317213a85967c703a4d526b2b97b20d331
cekidot <cekiddot@gmail.com>
2020-01-25 11:57:35 +0700

    use react spring

--- booklist/src/Components/BooksDetails.js
+++ booklist/src/Components/BooksDetails.js
@@ -1,34 +1,45 @@
-import React, { useContext } from 'react';
-import { useAnimate } from 'react-simple-animate';
+import React, { useContext, useState } from 'react';
+import { useTransition, animated, useSpring } from 'react-spring';
 import { BooksContext } from '../contexts/BooksContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BooksDetails = ({ book }) => {
-  const { isPurple, purple, dark } = useContext(ThemeContext);
-  const theme = isPurple ? purple : dark;
   const { dispatch } = useContext(BooksContext);
-  const { play, style, isPlay } = useAnimate({
-    start: {
-      opacity: 1,
-      transform: 'translateX(0px)',
-    },
-    end: { opacity: 0, transform: 'translateX(800px)' },
-    complete: () => {
-      dispatch({ type: 'REMOVE_BOOK', id: book.id });
-    },
+  const { isPurple, purple, dark } = useContext(ThemeContext);
+  const [state, toggle] = useState(true);
+  const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0 });
+  const bok = useTransition(book, (book) => book.id, {
+    from: { transform: 'translate3d(200px,400px,0)' },
+    enter: { transform: 'translate3d(0,0px,0)' },
+    leave: { transform: 'translate3d(-50%,0px,0)' },
   });
+  console.log(bok);
+  const theme = isPurple ? purple : dark;
   const removeBook = () => {
-    play(!isPlay);
+    toggle(!state);
+    setTimeout(() => {
+      dispatch({ type: 'REMOVE_BOOK', id: book.id });
+    }, 1000);
   };
-  return (
-    <li
-      onClick={removeBook}
-      style={{ ...style, background: theme.bgNav, color: theme.cl }}
-    >
-      <div className="title">{book.title}</div>
-      <div className="author">{book.author}</div>
-    </li>
-  );
+  return bok.map(({ item: { title, author }, props, key }) => {
+    return (
+      <animated.div
+        key={key}
+        style={{
+          ...props,
+          opacity: x.interpolate({ range: [0, 1], output: [0, 1] }),
+        }}
+      >
+        <li
+          onClick={removeBook}
+          style={{ background: theme.bgNav, color: theme.cl }}
+        >
+          <div className="title">{title}</div>
+          <div className="author">{author}</div>
+        </li>
+      </animated.div>
+    );
+  });
 };
 
 export default BooksDetails;
